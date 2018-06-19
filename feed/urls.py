f3805d6eaebf4626e71bfa81e794b5973b2bc6b3
chitranjali <chitranjali189@gmail.com>
2018-06-19 10:15:25 +0530

    added like functionality

--- feed/urls.py
+++ feed/urls.py
@@ -5,5 +5,6 @@ app_name = 'feed'
 urlpatterns = [
     url('userfeed/', views.CreatePostView.as_view(), name='userfeed'),
     url('post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
+    # url('post/(?P<pk>\d+)/like$', views.PostDetailView.as_view(), name='post_detail'),
     # url(r'^post/new/$',views.CreatePostView.as_view(),name='new_post'),
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from actions.models import Action
+# from django.views.mixins.ajaxformresponse import AjaxFormResponseMixin
 from django.views.generic import (TemplateView, ListView,
                                   DetailView, CreateView,
                                   UpdateView, DeleteView, RedirectView)
@@ -8,6 +9,9 @@ from django.urls import reverse_lazy
 from actions.utils import create_action
 import redis
 from django.conf import settings
+from braces.views import JSONResponseMixin, AjaxResponseMixin
+from django.http.response import HttpResponse
+import json
 
 r = redis.StrictRedis(host=settings.REDIS_HOST,
                       port=settings.REDIS_PORT,
@@ -40,7 +44,7 @@ class CreatePostView(CreateView):
         return super(CreatePostView, self).form_valid(form)
 
 
-class PostDetailView(DetailView):
+class PostDetailView(AjaxResponseMixin, DetailView):
     model = UserPost
     fields = ['post_body', 'image']
     context_object_name = 'post'
@@ -60,18 +64,58 @@ class PostDetailView(DetailView):
         try:
             total_views = r.incr('userpost:{}:views'.format(self.object.pk))
         except (redis.exceptions.ConnectionError,
-                    redis.exceptions.BusyLoadingError):
+                redis.exceptions.BusyLoadingError):
             pass
 
-    # # def dashboard(request):
-    #     # Display all actions by default
-    #     actions = Action.objects.all().exclude(user=request.user)
-    #     following_ids = request.user.following.values_list('id', flat=True)
-    #     if following_ids:
-    #         # If user is following others, retrieve only their actions
-    #         actions = actions.filter(user_id__in=following_ids).select_related('user', 'user__profile').prefetch_related(
-    # 'target')
-    #     actions = actions[:10]
-    #
-    #     return render(request, 'account/dashboard.html', {'section': 'dashboard',
-    #                                                       'actions': actions})
+    # def post_ajax(self, request, *args, **kwargs):
+    #     data = request.POST.items()  # form data
+    #     ctx = {'hi': 'hello'}
+    #     return self.render_json_response(ctx)
+
+    def post_ajax(self, request, *args, **kwargs):
+        obj = self.get_object()
+        print('ajax')
+        liked = False
+
+        if request.user in obj.likes.all():
+            liked = False
+            obj.likes.remove(request.user)
+        else:
+            liked = True
+            obj.likes.add(request.user)
+
+        response_data = {
+            "liked": liked,
+            "likes": str(obj.likes.count()),
+        }
+        print(response_data["likes"])
+        # if obj.likes:
+        #     response_data = {'result': "enabled"}
+        # else:
+        #     response_data = {'result': "disabled"}
+
+        return HttpResponse(json.dumps(response_data),
+                            content_type="application/json")
+
+        # class PostLikeView(AjaxFormResponseMixin, UpdateView):
+        #     # form_class = UpdateAuthorForm
+        #
+        #     def get_object(self, queryset=None):
+        #         return get_object_or_json404(Author, pk=self.kwargs['pk'])
+        #
+        #     def get_context_data(self, context):
+        #         context['success'] = True
+        #         return context
+
+        # # def dashboard(request):
+        #     # Display all actions by default
+        #     actions = Action.objects.all().exclude(user=request.user)
+        #     following_ids = request.user.following.values_list('id', flat=True)
+        #     if following_ids:
+        #         # If user is following others, retrieve only their actions
+        #         actions = actions.filter(user_id__in=following_ids).select_related('user', 'user__profile').prefetch_related(
+        # 'target')
+        #     actions = actions[:10]
+        #
+        #     return render(request, 'account/dashboard.html', {'section': 'dashboard',
+        #                                                       'actions': actions})
