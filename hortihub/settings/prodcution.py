4cfcb791c9f3b0f2710640808b930647a5c46798
chitranjali <chitranjali189@gmail.com>
2018-06-20 23:00:35 +0530

    added static_dirs

--- hortihub/settings/prodcution.py
+++ hortihub/settings/prodcution.py
@@ -29,10 +29,10 @@ DATABASES = {
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Why?
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 SOCIAL_AUTH_GITHUB_KEY = '5a1923402861bda4eb93'
 SOCIAL_AUTH_GITHUB_SECRET = 'ce9ae60d506f24914b473cb05cbb12ef8803b41a'
