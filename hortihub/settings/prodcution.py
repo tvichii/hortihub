6adc5a34108c5d830f465cdb8ecaa8d447bea26f
chitranjali <chitranjali189@gmail.com>
2018-06-20 23:19:30 +0530

    just

--- hortihub/settings/prodcution.py
+++ hortihub/settings/prodcution.py
@@ -30,9 +30,9 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Why?
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 STATICFILES_DIRS = [
     os.path.normpath(BASE_DIR, 'static'),
 ]
