e5adda82de11f1a04483403c2485ffd84a4ea1a4
chitranjali <chitranjali189@gmail.com>
2018-06-20 17:22:04 +0530

    changed production settings DB

--- hortihub/settings/prodcution.py
+++ hortihub/settings/prodcution.py
@@ -1,4 +1,5 @@
 from .base import *
+import dj_database_url
 
 ALLOWED_HOSTS = ['*','hortihub.herokuapp.com']
 
@@ -11,13 +12,17 @@ INSTALLED_APPS += (
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
@@ -27,15 +32,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Why?
-
-
-LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/feed/userfeed/'
-LOGOUT_REDIRECT_URL = '/'
-
-ABSOLUTE_URL_OVERRIDES = {
-    'auth.user': lambda u: reverse_lazy('accounts:userdetail', args=[u.pk])
-}
 
 SOCIAL_AUTH_GITHUB_KEY = '5a1923402861bda4eb93'
 SOCIAL_AUTH_GITHUB_SECRET = 'ce9ae60d506f24914b473cb05cbb12ef8803b41a'
